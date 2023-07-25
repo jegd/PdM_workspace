7469c04753d3c55c0ee00366ed1ff03258ce7037
Jesus Gonzales <jesus.e_97@hotmail.com>
2023-07-25 01:28:02 -0500

    Añadido ejercicio 4.1

---
+++ Ejercicio_4.1/Drivers/API/Src/Api_delay.c
@@ -0,0 +1,27 @@
+
+#include "Api_delay.h"
+void delayInit( delay_t * delay, tick_t duration ){
+	assert(delay!= NULL);
+ delay->duration= duration;
+ delay->running=false;
+}
+
+bool_t delayRead( delay_t * delay ){
+	assert(delay!= NULL);
+	if((delay->running)==false ){
+		delay->startTime = HAL_GetTick();
+		delay->running = true;
+	}
+
+	if(delay->running){
+		if((HAL_GetTick()-(delay->startTime))>=delay->duration){
+			delay->running = false;
+		}
+	}
+return delay->running;
+
+}
+void delayWrite( delay_t * delay, tick_t duration ){
+	assert(delay!= NULL);
+	delay->duration = duration;
+}
